d9736240a4e8f184d914d03c2255e570764c3ff0
wendall@hersheysmm.com <wendall@hersheysmm.com>
2018-11-17 18:14:38 -0500

    Add main git commit view.

--- TheTimeApp/WPFTimeAppForm.xaml.cs
+++ TheTimeApp/WPFTimeAppForm.xaml.cs
@@ -6,6 +6,7 @@ using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using LibGit2Sharp;
 using TheTimeApp.Controls;
 using TheTimeApp.TimeData;
 
@@ -19,6 +20,13 @@ namespace TheTimeApp
     {
         private System.Timers.Timer _timeTic;
         private System.Timers.Timer _detailsChanged;
+
+        /// <summary>
+        /// True if git commits are
+        /// visible instead of details.
+        /// </summary>
+        private bool _gitCommits;
+
         public WPFTimeAppForm()
         {
             InitializeComponent();
@@ -39,6 +47,8 @@ namespace TheTimeApp
 
             DayDetailsBox.Text = DataBaseManager.Instance.CurrentDay().Details;
             btn_SelectedUser.Content = TimeServer.SqlCurrentUser;
+            
+            btn_DetailsCommits.Visibility = AppSettings.Instance.GitEnabled ? Visibility.Visible : Visibility.Hidden;
 
             if (AppSettings.Instance.SqlEnabled != "true")
             {
@@ -52,6 +62,8 @@ namespace TheTimeApp
                 Start_Button.Margin = new Thickness(10,0,10,40);
                 Start_Button.Height = 85;
             }
+
+            LoadCommitMsgs();
             
             DataBaseManager.Instance.ConnectionChangedEvent += ConnectionChanged;
             DataBaseManager.Instance.UpdateChangedEvent += SqlUpdateChanged;
@@ -60,6 +72,17 @@ namespace TheTimeApp
             
             UpdateTime();
             UpdateGit();
+        }
+
+        private void LoadCommitMsgs()
+        {
+            if (!AppSettings.Instance.GitEnabled) return;
+
+            GitCommitsBox.Text = "";
+            foreach (GitCommit commit in GitManager.Instance.CommitsOnDate(DateTime.Today))
+            {
+                GitCommitsBox.Text += "-o- " +  commit.Message;
+            }
         }
 
         private void LoadUsers()
@@ -147,6 +170,7 @@ namespace TheTimeApp
             if (!AppSettings.Instance.GitEnabled) return;
             var commits = GitManager.Instance.CommitsOnDate(DateTime.Today);
             commits.ForEach(c => DataBaseManager.Instance.AddCommit(c));
+            LoadCommitMsgs();
         }
 
         private void UpdateTime()
@@ -231,6 +255,34 @@ namespace TheTimeApp
         {
             new SettingsWindow().ShowDialog();
             Init();// reinitialize
-        }        
+        }
+
+        private void btn_DetailsCommits_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleDetailsCommits();
+        }
+
+        private void ToggleDetailsCommits()
+        {
+            _gitCommits = !_gitCommits;
+            if (_gitCommits)
+            {
+                LoadCommitMsgs();
+                
+                DayDetailsBox.Visibility = Visibility.Hidden;
+                GitCommitsBox.Visibility = Visibility.Visible;
+                
+                ImageDetails.Visibility = Visibility.Hidden;
+                ImageCommits.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                GitCommitsBox.Visibility = Visibility.Hidden;
+                DayDetailsBox.Visibility = Visibility.Visible;
+                
+                ImageCommits.Visibility = Visibility.Hidden;
+                ImageDetails.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
