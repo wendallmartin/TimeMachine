bc09b570b749b6295b4e8886c65e251b2cc27b77
wendall@hersheysmm.com <wendall@hersheysmm.com>
2019-03-13 17:54:53 -0400

    Add Date selection to time edit window.

--- TheTimeApp/Controls/WpfTimeEdit.xaml.cs
+++ TheTimeApp/Controls/WpfTimeEdit.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using TheTimeApp.TimeData;
 
 namespace TheTimeApp.Controls
@@ -16,14 +17,34 @@ namespace TheTimeApp.Controls
 
             Time = time;
 
-            InTime.DateTime = time.TimeIn;
-            OutTime.DateTime = time.TimeOut;
+            InDatePicker.SelectedDate = InTime.DateTime = time.TimeIn;
+            OutDatePicker.SelectedDate = OutTime.DateTime = time.TimeOut;
         }
 
         private void Btn_SaveClick(object sender, RoutedEventArgs e)
         {
-            Time.TimeIn = InTime.DateTime;
-            Time.TimeOut = OutTime.DateTime;
+            if (InDatePicker.SelectedDate.HasValue && OutDatePicker.SelectedDate.HasValue)
+            {
+                int inYear = InDatePicker.SelectedDate.Value.Year;
+                int inMonth = InDatePicker.SelectedDate.Value.Month;
+                int inDay = InDatePicker.SelectedDate.Value.Day;
+                int inHour = InTime.DateTime.Hour;
+                int inMin = InTime.DateTime.Minute;
+                int inSec = InTime.DateTime.Second;
+                
+                int outYear = OutDatePicker.SelectedDate.Value.Year;
+                int outMonth = OutDatePicker.SelectedDate.Value.Month;
+                int outDay = OutDatePicker.SelectedDate.Value.Day;
+                int outHour = OutTime.DateTime.Hour;
+                int outMin = OutTime.DateTime.Minute;
+                int outSec = OutTime.DateTime.Second;
+            
+                DateTime timeIn = new DateTime(inYear, inMonth, inDay, inHour, inMin, inSec);
+                DateTime timeOut = new DateTime(outYear, outMonth, outDay, outHour, outMin, outSec);
+                
+                Time.TimeIn = timeIn;
+                Time.TimeOut = timeOut;    
+            }
 
             DialogResult = true;
             Close();
